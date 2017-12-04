eca9ff520a0a5449837cba089f8f6716ca4f00ba
Palmang Kevin Kombate <30642844+kevinkombate@users.noreply.github.com>
2017-12-04 13:46:29 -0500

    Exercice 2 - Complet
    Exercice 2 - Complet

--- NewEra Systems/src/Exercice2.java
+++ NewEra Systems/src/Exercice2.java
@@ -16,8 +16,7 @@ public class Exercice2 {
 
         System.out.println("Qu'avez-vous achetez aujourd'hui?");
         System.out.println();
-        System.out.println("Un article par ligne SVP");
-        System.out.println("'STOP' quand la liste est terminée");
+        System.out.println("Un article par ligne SVP et 'STOP' quand la liste est terminée");
         System.out.println("Exemple :");
         System.out.println("   ORANGE");
         System.out.println("   POMME");
@@ -57,11 +56,74 @@ public class Exercice2 {
 
     }
 
+
+    public void offresSimples(LinkedList L){
+
+
+        double prixFinal = 0.0;
+        int compteurNbrePomme = 0;
+        int compteurNbreOrange = 0;
+
+
+        for(Object a: L){
+
+            if(a.equals("POMME"))
+                compteurNbrePomme++;
+                if(compteurNbrePomme % 2 == 0)
+                    prixFinal = prixFinal;
+                else prixFinal+=pommePrix;
+
+            if(a.equals("ORANGE"))
+                compteurNbreOrange++;
+                if(compteurNbreOrange % 3 == 0)
+                    prixFinal = prixFinal;
+                else prixFinal += orangePrix;
+        }
+
+
+        System.out.println();
+        System.out.println("Le prix total est de: " + prixFinal +"$");
+
+
+
+    }
+
     public static void main (String [] args){
 
-        Exercice2 ex = new Exercice2();
-        LinkedList articles = ex.InputReader();
-        ex.shoppingCart(articles);
+        System.out.println("###############################################");
+        System.out.println("#                                             #");
+        System.out.println("#               SHOPPING CART                 #");
+        System.out.println("#                                             #");
+        System.out.println("###############################################");
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("NOS OFFRES HABITUELLES !!!");
+        System.out.println("** Achetez une pomme, obtenez-en une gratuitement **");
+        System.out.println("** Achetez deux Oranges, et obtenez la 3ième gratuitement. **");
+
+        System.out.println("---------------------------------------------------------------------");
+
+        System.out.println();
+
+
+        while(true) {
+            System.out.println("AVONS NOUS DES OFFRES CETTE SEMAINE ? (OUI/NON)");
+
+            Exercice2 ex = new Exercice2();
+            Scanner sc = new Scanner(System.in);
+
+
+            String input = sc.nextLine().toUpperCase();
+
+            if (input.equals("OUI"))
+                ex.offresSimples(ex.InputReader());
+            else if (input.equals("NON"))
+                ex.shoppingCart(ex.InputReader());
+
+        }
+
 
     }
 }
